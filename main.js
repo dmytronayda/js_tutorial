1b1f2c487b2999c05ad1e58bb1e6539a7e1a74f1
dmytronayda <dima.nayda.1993@gmail.com>
2019-07-12 16:32:46 +0200

    Add event resulting HTML appear on screen.

--- main.js
+++ main.js
@@ -1,10 +1,25 @@
 let Phrase = require("dnaida-palindrome"); 
 
-let string = prompt("Please enter a string for a palindrome testing:")
-let phrase = new Phrase(string); 
+function palindromeTester() {
+    let string = prompt("Please enter a string for a palindrome testing:")
+    let phrase = new Phrase(string); 
 
-if (phrase.palindrome()) {
-    alert(`"${phrase.content}" is a palindrome!`); 
-} else {
-    alert(`"${phrase.content}" is NOT a palindrome.`); 
-}; 
+    let palindromeResult = document.querySelector("#result"); 
+    if (phrase.palindrome()) {
+        palindromeResult.innerHTML = `"${phrase.content}" is a palindrome!`; 
+    } else {
+        palindromeResult.innerHTML = `"${phrase.content}" is NOT a palindrome.`; 
+    }; 
+};
+
+
+document.addEventListener("DOMContentLoaded",function () {
+    let form = document.querySelector("#palindromeTester"); 
+    form.addEventListener("submit", function() {
+        palindromeTester(); 
+        return false;
+    }); 
+});  
+
+
+
